f4beb3e815a41bb31ba4ec5bab052c5d47f02ba2
xuaanquynh04 <xuaanquynh04@gmail.com>
2022-12-02 09:22:38 +0700

    Initial commit

---
+++ ham.py
@@ -0,0 +1,12 @@
+def xuly(a,b):
+    if a*a==b or b*b==a:
+        tong=s+a+b
+    return tong
+n=int(input("ban muon nhap bao nhieu cap "))
+i=1
+s=0
+for i in range(1,n+1):
+    a = int(input("nhap so a "))
+    b = int(input("nhap so b "))
+    tong=xuly(a,b)
+print("tong la " + str(tong))
